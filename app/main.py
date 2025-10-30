d005b58284f6f5be9035db5945fd3085f2cc0031
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-10-30 10:21:17 +0200

    Changed type hints again, cannot delete 18 and 19 lines due to error

--- app/main.py
+++ app/main.py
@@ -18,18 +18,17 @@ class Distance:
         else:
             return Distance(self.km + other)
 
-    def __iadd__(self, other: float | int | Distance) \
-            -> float | int | Distance:
+    def __iadd__(self, other: float | int | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: float) -> float | Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> float | Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round((self.km / other), 2))
 
     def __lt__(self, other: Distance | float | int) -> bool:
